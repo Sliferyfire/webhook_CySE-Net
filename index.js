60840f4dc39693cdf1a5f77a552e997cd90f52e0
Sliferyfire <sliferyfire@gmail.com>
2024-07-05 00:52:57 -0600

    prueba 16

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const functions = require('firebase-functions');
 const {WebhookClient, Payload} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 const { log } = require('firebase-functions/logger');
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail', // Puedes usar cualquier servicio de correo (e.g., 'yahoo', 'hotmail', 'smtp')
+  auth: {
+    user: 'sliferyfire@gmail.com', // Tu correo electrónico
+    pass: 'aaronpro' // Tu contraseña (o mejor, una contraseña de aplicación)
+  }
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -20,10 +29,7 @@ app.post('/webhook', express.json(), function (req, res) {
   }
   
   function webhookPrueba(agent) {
-    // console.log('webhook prueba');
     agent.add(`Hola desde el webhook`); 
-    // agent.add(`Hola desde el webhook 2`); 
-    
     const payloadJson = {
         "richContent": [
           [
@@ -38,15 +44,30 @@ app.post('/webhook', express.json(), function (req, res) {
           ]
         ]
       };
-    // const payload = new Payload(agent.WEB, payloadJson, { rawPayload: true, sendAsMessage: true });
-    agent.add(new Payload(agent.ACTIONS_ON_GOOGLE, payloadJson, { rawPayload: true, sendAsMessage: true }));
+    agent.add(new Payload(agent.UNSPECIFIED, payloadJson, { rawPayload: true, sendAsMessage: true }));
   }
 
   function agendarMantenimientoCorreo(agent) {
     const context = agent.context.get('horamantenimiento');
     const locationOriginal = context.parameters['location.original'];
     const dateTimeOriginal = context.parameters['date-time.original'][0];
-    console.log(`Mantenimiento agendado para: ${dateTimeOriginal} en ${locationOriginal}`);
+    // console.log(`Mantenimiento agendado para: ${dateTimeOriginal} en ${locationOriginal}`);
+    let mailOptions = {
+      from: 'sliferyfire@gmail.com',
+      to: 'sliferyfire@gmail.com',
+      subject: 'Agenda de mantenimiento',
+      text: 'Manetnimiento en la fecha y hora: ' + dateTimeOriginal + ' en ' + locationOriginal,
+      // html: '<h1>Contenido del correo en HTML</h1>'
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Correo enviado: ' + info.response);
+    });
+
+    agent.add(`Mantenimiento agendado para: ${dateTimeOriginal} en ${locationOriginal}`);
+    agent.add(`Si necesita más ayuda, no dude en preguntar o comunicarse con un asesor.`);
     const payloadJson = {
       "richContent": [
         [
@@ -61,7 +82,6 @@ app.post('/webhook', express.json(), function (req, res) {
         ]
       ]
     };
-    // agent.add(`Webhook: Mantenimiento agendado para: ${dateTimeOriginal} en ${locationOriginal}`);
     agent.add(new Payload(agent.UNSPECIFIED, payloadJson, { rawPayload: true, sendAsMessage: true }));
   }
  
