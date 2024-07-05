932e4685ea114a5a3b56d786bcbf6e029e4f9a3b
Sliferyfire <sliferyfire@gmail.com>
2024-07-04 23:50:40 -0600

    prueba 10

--- index.js
+++ index.js
@@ -21,26 +21,27 @@ app.post('/webhook', express.json(), function (req, res) {
   
   function webhookPrueba(agent) {
     // console.log('webhook prueba');
-    // agent.add(`Hola desde el webhook`); 
+    agent.add(`Hola desde el webhook`); 
+    agent.add(`Hola desde el webhook 2`); 
     
-    const payloadJson = {
-      "richContent": [
-        [
-          {
-            "type": "description",
-            "title": "Computacion y servicio 🖥️",
-            "text": [
-              `Mantenimiento agendado para:`,
-              `Ubicación:`,
-              "Telefono: 4271090104",
-              "Email: servicio@computacionyservicio.mx"
-            ]
-          }
-        ]
-      ]
-    };
-    const payload = new Payload(agent.WEB, payloadJson, { rawPayload: true, sendAsMessage: true });
-    agent.add(payload);
+    // const payloadJson = {
+    //   "richContent": [
+    //     [
+    //       {
+    //         "type": "description",
+    //         "title": "Computacion y servicio 🖥️",
+    //         "text": [
+    //           `Mantenimiento agendado para:`,
+    //           `Ubicación:`,
+    //           "Telefono: 4271090104",
+    //           "Email: servicio@computacionyservicio.mx"
+    //         ]
+    //       }
+    //     ]
+    //   ]
+    // };
+    // const payload = new Payload(agent.WEB, payloadJson, { rawPayload: true, sendAsMessage: true });
+    // agent.add(payload);
   }
 
   function agendarMantenimientoCorreo(agent) {
